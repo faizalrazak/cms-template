b2aa34991c269f6fbb7d1d4702db419d3940ff5a
faizalrazak <mohamadfaizalrazak@gmail.com>
2017-09-14 12:26:57 +0800

    first commit

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,92 @@
+<!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+
+<!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          {{ HTML::image('img/user2-160x160.jpg', 'User Image',  array('class' => 'img-circle')) }}
+        </div>
+        <div class="pull-left info">
+          <p>Alexander Pierce</p>
+          <!-- Status -->
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+
+      <!-- search form (Optional) -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <span class="input-group-btn">
+              <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+              </button>
+            </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+
+      <!-- sidebar menu: : style can be found in s
+      idebar.less -->
+      <ul class="sidebar-menu " data-widget="tree">
+        <li class="header">MAIN NAVIGATION</li>
+        
+        <li class="treeview menu-open ">
+          <a href="">
+            <i class="fa fa-dashboard"></i> <span class="text-info">Dashboard</span>
+          </a>
+        </li>
+
+        <li class="treeview">
+          <a href="">
+            <i class="fa fa-files-o"></i><span class="text-info">User Management</span>
+          </a>
+        </li>
+
+        <li class="treeview">
+          <a href="">
+            <i class="fa fa-share"></i> <span class="text-info">Order Placement Management</span>
+          </a>
+        </li>
+
+        <li class="treeview">
+          <a href="">
+            <i class="fa fa-share"></i> <span class="text-info">Delivery Management</span>
+          </a>
+        </li>
+
+        <li class="treeview">
+          <a href="">
+            <i class="fa fa-share"></i> <span class="text-info">Invoice Management</span>
+          </a>
+        </li>
+
+        <li class="treeview">
+          <a href="">
+            <i class="fa fa-share"></i> <span class="text-info">Payment Management</span>
+          </a>
+        </li>
+
+        <li class="treeview">
+          <a href="">
+            <i class="fa fa-pie-chart"></i><span class="text-info">Service Management</span>
+          </a>
+        </li>
+
+        <li class="treeview">
+          <a href="">
+            <i class="fa fa-calendar"></i> <span class="text-info">News & Promotion</span>
+          </a>
+        </li>
+
+        <li class="treeview">
+          <a href="">
+            <i class="fa fa-calendar"></i> <span class="text-info">Status</span>
+          </a>
+        </li>
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.master')
+@section('style')
+@endsection
+@section('content')
+
+
+@endsection
